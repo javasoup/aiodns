45478480770478d9684c82dc4df91042a98043c5
Nuno Pereira <nuno01@gmail.com>
2021-04-16 14:56:05 -0400

    Set log level to INFO for file

--- dnsproxy.go
+++ dnsproxy.go
@@ -151,6 +151,7 @@ func run(options Options) {
 		log.SetLevel(log.DEBUG)
 	}
 	if options.LogOutput != "" {
+		log.SetLevel(log.INFO)
 		file, err := os.OpenFile(options.LogOutput, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 		if err != nil {
 			log.Fatalf("cannot create a log file: %s", err)
